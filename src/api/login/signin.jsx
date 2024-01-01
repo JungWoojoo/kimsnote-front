6eaeea22d7abd12a809f01bb8c5966a0dfabd1dc
JungWoojoo <jwj5045@gmail.com>
2024-01-02 00:03:27 +0900

    fix: 구글 oauth2 유저 정보 조회

---
+++ src/api/login/signin.jsx
@@ -0,0 +1,12 @@
+import api from "../index";
+
+const apiUrl = `${process.env.REACT_APP_API_URL}/member`
+
+export const signIn = async (params) =>
+    await api.post(`${apiUrl}/login`, params)
+        .then((response) => {return response});
+
+//
+// export const oauthSignIn = async () =>
+//     await api.get(`https://accounts.google.com/o/oauth2/v2/auth?client_id=${googleClientId}&redirect_uri=http://localhost:48080/api/oauth2/callback/google&response_type=code&scope=profile`)
+//         .then((response) => {return response});
