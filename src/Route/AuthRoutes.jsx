bacaa53d03d9f074f0879067be48a5ddb2d3d06c
JungWoojoo <jwj5045@gmail.com>
2024-01-11 23:09:00 +0900

    fix: 수정

--- src/Route/AuthRoutes.jsx
+++ src/Route/AuthRoutes.jsx
@@ -1,5 +1,10 @@
 import Callback from "../Auth/Callback";
+import Join from "../Auth/Join";
 
 export const authRoutes = [
+  //join
+  { path: `${process.env.PUBLIC_URL}/join`, Component: <Join /> },
+
+  //oauth 이후 콜백
   { path: `${process.env.PUBLIC_URL}/oauth2/redirect/:registrationId/:token`, Component: <Callback /> },
 ];
--- src/Route/PrivateRoute.jsx
+++ src/Route/PrivateRoute.jsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import loginStore from "../Store/login/Login";
+import React, {useEffect, useState} from "react";
+import {Navigate, Outlet} from "react-router-dom";
 
 const PrivateRoute = () => {
   const [login, setLogin] = useState(JSON.parse(localStorage.getItem("member")) ? JSON.parse(localStorage.getItem("member")) : false);
   const [authenticated, setAuthenticated] = useState(false);
-  const {isLogin} = loginStore()
 
   useEffect(() => {
     setAuthenticated(JSON.parse(localStorage.getItem("authenticated")));
@@ -13,7 +11,7 @@ const PrivateRoute = () => {
     setLogin(JSON.parse(localStorage.getItem("member")));
   }, [setLogin]);
 
-  return isLogin !== false || authenticated !== false ? <Outlet /> : <Navigate exact to={`${process.env.PUBLIC_URL}/login`} />;
+  return login !== false || authenticated !== false ? <Outlet /> : <Navigate exact to={`${process.env.PUBLIC_URL}/login`} />;
 };
 
 export default PrivateRoute;
